27362e206047bf6e30bc40e1d834934b269bc3dd
Kumar <kumarabhinavansh652@gmail.com>
2023-09-08 17:11:55 +0530

    server.js file updated

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,20 @@ const MongoClient = require('mongodb').MongoClient;
 const mongoUrl = 'mongodb://localhost:9000/moviesdb';
 
 
-const db = require("/models");
-db.mongoose
-    .connect(db.url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log("Connected to the database!");
+// const db = require("/models");
+// db.mongoose
+//     .connect(db.url, {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     })
+//     .then(() => {
+//         console.log("Connected to the database!");
 
-    })
-    .catch(err => {
-        console.log("Cannot connect to the database!", err);
-        process.exit();
-    });
+//     })
+//     .catch(err => {
+//         console.log("Cannot connect to the database!", err);
+//         process.exit();
+//     });
 
 
 const server = http.createServer(async (req, res) => {
@@ -26,22 +26,37 @@ const server = http.createServer(async (req, res) => {
 
     if (method == 'GET') {
         if (path == '/movies') {
-            const client = new MongoClient(mongoUrl, { userNewUrlParser: true });
+            const client = new MongoClient(mongoUrl, { useNewUrlParser: true });
             await client.connect();
             const db = client.db('moviesdb');
             const moviesCollection = db.collection('movies');
             const movieData = await moviesCollection.find().toArray();
-            res.writeHead(200, { 'contect-type': 'application/json' });
+            res.writeHead(200, { 'content-type': 'application/json' });
             res.end(JSON.stringify(movieData));
         }
     }
 
-    if (method =='GET') {
+    if (method == 'GET') {
         if (path == '/artists') {
-            const client = new MongoClient(mongoUrl, {userNewUrlParser: true});
+            const client = new MongoClient(mongoUrl, { useNewUrlParser: true });
             await client.connect();
             const db = client.db('moviesdb');
-            
+            const artistsCollection = db.collection('artists');
+            const artistData = await artistsCollection.find().toArray();
+            res.writeHead(200, { 'content-type': 'application/json' });
+            res.end(JSON.stringify(artistData));
+        }
+    }
+
+    if (method == 'GET') {
+        if (path == '/genres') {
+            const client = new MongoClient(mongoUrl, { useNewUrlParser: true });
+            await client.connect();
+            const db = client.db('genresdb');
+            const genresCollection = db.collection('genres');
+            const genresData = await genresCollection.find().toArray();
+            res.writeHead(200, { 'content-type': 'application/json' });
+            res.end(JSON.stringify(genresData));
         }
     }
 })
