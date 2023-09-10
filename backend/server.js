c52e6cb39490168d81790757683f7293739cedf6
Kumar <kumarabhinavansh652@gmail.com>
2023-09-10 10:33:38 +0530

    folder str changed

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,20 @@ const MongoClient = require('mongodb').MongoClient;
 const mongoUrl = 'mongodb://localhost:9000/moviesdb';
 
 
-// const db = require("/models");
-// db.mongoose
-//     .connect(db.url, {
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     })
-//     .then(() => {
-//         console.log("Connected to the database!");
+const db = require("/models");
+db.mongoose
+    .connect(db.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Connected to the database!");
 
-//     })
-//     .catch(err => {
-//         console.log("Cannot connect to the database!", err);
-//         process.exit();
-//     });
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
 
 
 const server = http.createServer(async (req, res) => {
